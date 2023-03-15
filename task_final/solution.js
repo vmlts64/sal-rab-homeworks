5a4407b52e2aae8d38125423c4066e2fb5e55475
Neto test <vmlts64@gmail.com>
2023-03-15 22:44:00 +0300

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,16 +38,16 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({'title':goods[i].title, 'count':goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв. ' + address.flat;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
