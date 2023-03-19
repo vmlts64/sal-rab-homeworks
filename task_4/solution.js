fa60a4ff463c80666d3c1b183388c56593c7cea2
Neto test <vmlts64@gmail.com>
2023-03-19 22:32:20 +0300

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -14,7 +14,7 @@ function renderProductsCards (json) {
     clearProducts ();
     let products = parseProducts (json);
     let length = products.length;
-    for (let i=0; i < (length - 1); i++) {
+    for (let i=0; i <= (length - 1); i++) {
     addProduct (products[i]);        
     }
 }
